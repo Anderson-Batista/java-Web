a1453251afb745bd833b791656340950bf1177ef
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-04-29 08:59:49 -0300

    CRUD CREATE - JavaBeans PART 1

---
+++ WebContent/scripts/validador.js
@@ -0,0 +1,22 @@
+/**
+ * Validação de formulário
+ * @author Anderson Batista
+ */
+
+function validar() {
+	let nome = frmContato.nome.value;
+	let fone = frmContato.fone.value;
+	if(nome === ""){
+		alert("Preencha o campo nome");
+		frmContato.nome.focus();
+		return false;
+	}
+	else if(fone === ""){
+		alert("Preencha o campo fone");
+		frmContato.nome.focus();
+		return false;
+	}
+	else{
+		document.forms["frmContato"].submit();
+	}
+}
